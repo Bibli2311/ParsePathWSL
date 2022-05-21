12b5ac94f972029d6aec3c16b452b276faba4964
oscarva@stud.ntnu.no <oscarva@stud.ntnu.no>
2022-05-21 12:54:43 +0200

    removed some uneccessary outputs from script

--- README.md
+++ README.md
@@ -6,5 +6,9 @@ A simple Python script which parses a path from windows into a format which is c
 python3 parsePathWSL.py 'C:\Users\ubuntu\Music\'
 Then the script will return a WSL compatible path like this: /mnt/c/Users/ubuntu/Music/
 
+One way to use this script is to do this: 
+cd "$(python3 parsePathWSL.py '<Windows path>')"
+This will use the result of the script directly with the cd command.
+
 NOTE: The character "'" must be placed at the start and end of the argument in order for it to work.
 This has something to do with how the terminal parses backslashes.
--- parsePathWSL.py
+++ parsePathWSL.py
@@ -6,8 +6,6 @@ except IndexError:
     print("Error: Pass in an argument for specifying path")
     print("For example: python3 fixPath.py C:\\Users\\oscar\\Music\\")
     sys.exit(1)
-   
-print("you entered: " + path)
 
 backslash = ord("\\")      #ascii value of backslash
 parsedPath = ""
@@ -17,7 +15,6 @@ for element in path:
         element = "/"            
     parsedPath += element           #replaces backslash with forward slash
 
-print(parsedPath)
 
 resultPath = "/mnt/c" + parsedPath[2:] #add /mnt/c to string, and remove "C:/" from the parsed path
 print(resultPath)
